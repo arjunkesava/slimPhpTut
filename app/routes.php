813f168b0a2d06b1effde8062984a1547baa841e
Arjun <arjun.kesava@gmail.com>
2018-08-18 15:36:25 +0530

    Added Basic View(twig) for Slim API '

--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,7 @@
 <?php
 
-$app->get('/home', function(){
-    return "Home";
+$app->get('/home', function($request, $response){
+    return $this->view->render($response,'home.twig');
 });
 
 
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,12 +1,29 @@
 <?php
-session_start();
 
 require __DIR__ .'/../vendor/autoload.php';
 
 $app = new \Slim\App([
     'settings' => [
-        'displayErrorDetails' => true,
+		'displayErrorDetails' => true,
+		'debug' => true,
+		'determineRouteBeforeAppMiddleware' => true,
+		'addContentLengthHeader' => false
     ]
 ]);
+
+$container = $app->getContainer();
+
+$container['view'] = function ($container) {
+	$view = new \Slim\Views\Twig(__DIR__ . '/../resources/views',[
+		'cache' => false,
+	]);
+
+	$view->addExtension(new \Slim\Views\TwigExtension(
+		$container->router,
+		$container->request->getUri()
+	));
+
+	return $view;
+};
 
 require __DIR__.'/../app/routes.php';
